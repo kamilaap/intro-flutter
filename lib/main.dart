943d09bbb81236c77879bfdf153e30135bf91cf5
Kamila Putri Herlambang <129238631+kamilaap@users.noreply.github.com>
2024-07-27 12:19:22 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,91 +1,40 @@
 import 'package:flutter/material.dart';
 
-import './hal_komputer.dart' as komputer;
-import './hal_headset.dart' as headset;
-import './hal_radio.dart' as radio;
-import './hal_smartphone.dart' as hp;
-
 void main() {
-  runApp(MaterialApp(
-    title: "Tab Bar",
-    home: Home(),
-  ));
+  runApp(
+    MaterialApp(
+      home: MyApp(),
+    ),
+  );
 }
 
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
-  late TabController controller;
-
-  @override
-  void initState() {
-    controller = TabController(vsync: this, length: 4);
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        backgroundColor: Colors.amber,
-        title: Text("Daftar Elektronik"),
-        bottom: new TabBar(
-          controller: controller,
-          tabs: <Widget>[
-            new Tab(
-              icon: new Icon(Icons.computer),
-              text: "Komputer",
-            ),
-            new Tab(
-              icon: new Icon(Icons.headset),
-              text: "Headset",
-            ),
-            new Tab(
-              icon: new Icon(Icons.radio),
-              text: "Radio",
-            ),
-            new Tab(
-              icon: new Icon(Icons.smartphone),
-              text: "Smartphone",
-            ),
-          ],
-        ),
+    return Scaffold(
+
+      backgroundColor: Colors.grey,
+
+      appBar: AppBar(
+        title: Text('My App'),
+        backgroundColor: Colors.blue,
+        leading: const Icon(Icons.home),
       ),
-      body: new TabBarView(controller: controller, children: <Widget>[
-        new komputer.Komputer(),
-        new headset.Headset(),
-        new radio.Radio(),
-        new hp.Smartphone()
-      ]),
-      bottomNavigationBar: new Material(
-        color: Colors.amber,
-        child: new TabBar(
-          controller: controller,
-          tabs: <Widget>[
-            new Tab(
-              icon: new Icon(Icons.computer),
-            ),
-            new Tab(
-              icon: new Icon(Icons.headset),
-            ),
-            new Tab(
-              icon: new Icon(Icons.radio),
-            ),
-            new Tab(
-              icon: new Icon(Icons.smartphone),
-            ),
-          ],
-        ),
+      
+      body: Container(
+        color: Colors.red,
+        width: double.infinity,
+        height: 90.0,
+        child: const Text(
+          'Selamat Datang!',
+          style: TextStyle(
+            fontFamily: "Times New Roman",
+            fontSize: 30.0,
+            fontWeight: FontWeight.bold,
+          ),
+          ),
       ),
+  
     );
   }
 }
